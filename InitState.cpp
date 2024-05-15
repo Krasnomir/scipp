c987740eae6f5c98f30790d7bf9cabb737d234db
mkrzywania <mkrzywania@SCI-INF305-06>
2024-05-15 09:11:32 +0200

    Triangles

--- InitState.cpp
+++ InitState.cpp
@@ -3,35 +3,51 @@
 #include "InitState.h"
 #include "Entity.h"
 
+#include <SFML/Graphics.hpp>
+
 #include <iostream>
 
-void InitState::init()
+void InitState::addTriangle(sf::Vector2f pos)
 {
-	sf::VertexArray triangle(sf::TriangleStrip, 4);
+	sf::VertexArray triangle(sf::TriangleStrip, 3);
 
 	triangle[0].position = sf::Vector2f(0.f, 0.f);
-	triangle[1].position = sf::Vector2f(0.f, 200.f);
-	triangle[2].position = sf::Vector2f(200.f, 0.f);
-	triangle[2].position = sf::Vector2f(200.f, 200.f);
+	triangle[1].position = sf::Vector2f(0.f, 100.f);
+	triangle[2].position = sf::Vector2f(100.f, 0.f);
 
-	triangle[0].color = sf::Color::Yellow;
-	triangle[1].color = sf::Color::Green;
-	triangle[2].color = sf::Color::Red;
-	triangle[3].color = sf::Color::Magenta;
+	triangle[0].color = sf::Color::Red;
+	triangle[1].color = sf::Color::Yellow;
+	triangle[2].color = sf::Color::Green;
 
 	Entity entity(triangle);
-	
-	entity.setPosition(200.f, 200.f);
+
+	entity.setPosition(pos.x, pos.y);
 	entity.setRotation(30.f);
+
+	entity.setOrigin(50.f, 50.f);
 
 	this->components.push_back(entity);
 }
 
+void InitState::init()
+{
+	int rate = 50;
+
+	for (int i = 0; i < 800; i += rate)
+	{
+		for (int j = 0; j < 600; j += rate)
+		{
+			this->addTriangle(sf::Vector2f(i, j));
+		}
+	}
+}
+
 void InitState::update(sf::Time elapsed)
 {
+	int degPerSec = 180;
+
 	for (auto& entity : this->components)
 	{
-		int degPerSec = 45;
 		entity.rotate(degPerSec * (elapsed.asMilliseconds() / 1000.f));
 	}
 }
--- InitState.h
+++ InitState.h
@@ -1,14 +1,19 @@
 #pragma once
 
+#include "State.h"
+#include "Entity.h"
+
 #include <SFML/Graphics.hpp>
 #include <SFML/System.hpp>
 
-#include "State.h"
+#include <vector>
 
 class InitState : public State
 {
 	void init();
 	void update(sf::Time elapsed);
 public:
+	void addTriangle(sf::Vector2f pos);
+
 	InitState(StateManager* stateManager);
 };
