7c34048377a67ab8f50503ca9be66f2d68eac0c2
Krasnomir <130172058+Krasnomir@users.noreply.github.com>
2024-12-08 20:55:00 +0100

    Delete app directory

--- app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-class Post(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-    title = models.CharField(max_length=50)
-    body = models.TextField()
-    date = models.DateTimeField()
